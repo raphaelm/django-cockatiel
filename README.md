d21e753f84c4e2b86c0af95c448b70977d716abe
Raphael Michel <mail@raphaelmichel.de>
2016-04-03 11:54:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+django-cockatiel
+================
+
+This is a django storage backend that makes use of the cockatiel distributed
+file storage. Please read the [cockateal documentation](https://cockatiel.readthedocs.org/)
+if you are unsure about what this is.
+
---
+++ setup.py
@@ -0,0 +1,37 @@
+from codecs import open
+from os import path
+
+from setuptools import find_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='django-cockatiel',
+    version='0.0.1',
+    description='Simple replicated file storage',
+    long_description=long_description,
+    url='https://github.com/raphaelm/django-cockatiel',
+    author='Raphael Michel',
+    author_email='mail@raphaelmichel.de',
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='django file storage media backend replicated',
+    extras_require={
+        'dev': ['pytest', 'flake8'],
+    },
+
+    packages=find_packages(exclude=['tests', 'tests.*', 'docs', 'docs.*']),
+    include_package_data=True,
+)
